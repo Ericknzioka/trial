eb8e206aaa4ad7d7a88357af6dcc46292a8be551
Ericknzioka <145925800+Ericknzioka@users.noreply.github.com>
2024-03-22 16:02:39 +0300

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,16 @@
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0 :
+        print(f"{num} - phizzbuzz") 
+    elif num % 3 == 0 :
+        print(f"{num} - phizz")
+    elif num % 5 == 0:
+        print(f"{num} - buzz")
+    
+            
+
+     
+        
+
+
+
+ 
